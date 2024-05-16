835b313148d6b00281dfa7b8ae66c4aeb5d117f8
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-16 08:55:09 +0200

    update

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDdVkaoblui6CXZAUDeMJHItHe_MYpWM_I",
+    authDomain: "signupform-html.firebaseapp.com",
+    projectId: "signupform-html",
+    storageBucket: "signupform-html.appspot.com",
+    messagingSenderId: "93324160778",
+    appId: "1:93324160778:web:2cc6376bcc9b0e29dc54f3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
