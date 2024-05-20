85dd8bf1d5228748eada1b7ea14fb9afec4a0d99
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-20 16:33:34 +0200

    update

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPasswor, signInWithEmailPasswor, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailPassword, onAuthStateChanged, signOut as firebaseSignOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configura
+// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyBMenG6Vqk9Hpjuu-dWg9FmMRndJ1CE1n4",
     authDomain: "signupwithfirebasejs.firebaseapp.com",
@@ -17,3 +17,29 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const signup = document.getElementById('signup');
+const authPart = document.getElementById('authPart');
+const signOutButton = document.getElementById('signOutButton');
+
+authPart.style.display = 'none';
+
+const signUp = async () => {
+    const signUpEmail = email.value;
+    const signUpPassword = password.value;
+    createUserWithEmailAndPassword(auth, signUpEmail, signUpPassword).then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        alert("well done");
+    }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        alert("try again");
+    });
+};
+
+signup.addEventListener('click', signUp);
+signUpBtn.addEventListener('click', signUp);
